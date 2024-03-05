e6c98d81dc472bf9838824d143b3215cab987d6c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-05 11:31:54 +0100

    Handle error

--- index.ts
+++ index.ts
@@ -2,13 +2,17 @@ import { TicketManager } from "./ticketManager";
 import { DatabaseService } from "./databaseService";
 import { EmailService } from "./emailService";
 
-const ticketManager = new TicketManager(3);
+const ticketManager = new TicketManager(0);
 const emailService = new EmailService();
 const databaseService = new DatabaseService();
 
 ticketManager.on("buy", (email: string, price: number, timestamp: Date) => {
   emailService.send(email);
   databaseService.save(email, price, timestamp);
+});
+
+ticketManager.on("error", () => {
+  console.log("Gracefully handled the error");
 });
 
 ticketManager.buy("test1.com", 100);
